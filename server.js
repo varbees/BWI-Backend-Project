61ea56219609faa70df2bc5a7d9366cf79121f6c
varbees <harshavar968@gmail.com>
2023-12-29 23:14:39 +0530

    Setting up folder strucutre(mvc) and default environment for development

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Build With Innovation');
+import express from 'express';
+import { __port__ } from './config/constants.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Build With Innovation 1.0.0');
+});
+
+app.listen(__port__, () => {
+  console.log(`BWI-API listening on ${__port__}`);
+});
