ffbb166c0e7902cd210651e9c7c3a22f35af9222
varbees <harshavar968@gmail.com>
2023-12-29 23:26:13 +0530

    Basic error handling setup and an async handler

---
+++ middleware/errorHandler.js
@@ -0,0 +1,18 @@
+import { __prod__ } from '../config/constants.js';
+
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  let statusdCode = res.statusdCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+  res.status(statusdCode).json({
+    message,
+    stack: __prod__ ? '' : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { __port__ } from './config/constants.js';
+import { errorHandler, notFound } from './middleware/errorHandler.js';
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Build With Innovation 1.0.0');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(__port__, () => {
   console.log(`BWI-API listening on ${__port__}`);
